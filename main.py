1f2b6de9b0fe0de1921724505208ee6308167dd5
konstivo <konstivo2@gmail.com>
2023-05-28 12:44:02 -0400

    Main file added

---
+++ main.py
@@ -0,0 +1,37 @@
+# imports
+import numpy as np
+import pandas as pd
+from sklearn import tree
+from sklearn import preprocessing
+
+dataset = np.array([
+    ['yes', 'no', 'no', 'yes', 'some', '$$$',
+        'no', 'yes', 'french', '0-10', 'yes'],
+    ['yes', 'no', 'no', 'yes', 'full', '$', 'no', 'no', 'thai', '30-60', 'no'],
+    ['no', 'yes', 'no', 'no', 'some', '$', 'no', 'no', 'burger', '0-10', 'yes'],
+    ['yes', 'no', 'yes', 'yes', 'full', '$', 'yes', 'no', 'thai', '10-30', 'yes'],
+    ['yes', 'no', 'yes', 'no', 'full', '$$$', 'no', 'yes', 'french', '>60', 'no'],
+    ['no', 'yes', 'no', 'yes', 'some', '$$',
+        'yes', 'yes', 'italian', '0-10', 'yes'],
+    ['no', 'yes', 'no', 'no', 'none', '$', 'yes', 'no', 'burger', '0-10', 'no'],
+    ['no', 'no', 'no', 'yes', 'some', '$$', 'yes', 'yes', 'thai', '0-10', 'yes'],
+    ['no', 'yes', 'yes', 'no', 'full', '$', 'yes', 'no', 'burger', '>60', 'no'],
+    ['yes', 'yes', 'yes', 'yes', 'full', '$$$',
+        'no', 'yes', 'italian', '10-30', 'no'],
+    ['no', 'no', 'no', 'no', 'none', '$', 'no', 'no', 'thai', '0-10', 'no'],
+    ['yes', 'yes', 'yes', 'yes', 'full', '$',
+        'no', 'no', 'burger', '30-60', 'yes'],
+
+])
+print(dataset)
+
+df2 = pd.DataFrame(dataset,
+                   columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'Will Wait'])
+blankIndex = [''] * len(df2)
+df2.index = blankIndex
+df2
+
+X = dataset[:, 0:10]
+y = dataset[:, 10]
+print(X)
+print(y)
